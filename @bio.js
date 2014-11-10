a62f3d7421086c0269fb6eb22c9a48acea2256b8
Igor Cordon <relatur@gmail.com>
2014-11-11 00:17:00 +0400

    ProteinTranslation, PeptideEncoding, Find

--- @bio.js
+++ @bio.js
@@ -16,6 +16,7 @@ exports.HammingDistance = function(stringX, stringY)
 //
 // Example : 
 // MinimumSkew('ATGCCTTACCTAGATGCAATGA');
+// Skew('ATGCCTTACCTAGATGCAATGA');
 exports.MinimumSkew = function(string)
 {
 	for(var i = 0, s = 0, min = 0, place = [ 1 ]; i < string.length; i++)
@@ -26,6 +27,16 @@ exports.MinimumSkew = function(string)
 		if(s <  min) place = [ i + 1 ], min = s;
 	}
 	return place;
+}
+exports.Skew = function(string)
+{
+	for(var i = 0, s = 0, trace = []; i < string.length; i++)
+	{
+		if(string[i] == "C") s--;
+		if(string[i] == "G") s++;
+		trace.push(s);
+	}
+	return trace;
 }
 
 // Frequent Words with Mismatches and Reverse Complements Problem: Find the most frequent k-mers (with mismatches and reverse complements) in a DNA string.
@@ -93,3 +104,55 @@ exports.Mismatches = function(data, d)
 	}
 	return d > 1 ? exports.Mismatches(variables, d - 1) : variables;
 }
+
+// Protein Translation Problem: Translate an RNA string into an amino acid string.
+//      Input: An RNA string Pattern and the array GeneticCode.
+//      Output: The translation of Pattern into an amino acid string Peptide.
+//
+// Example : 
+// ProteinTranslation('AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA');
+exports.ProteinTranslation = function(string)
+{
+	var table = { 'AAA':'K', 'AAC':'N', 'AAG':'K', 'AAU':'N', 'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACU':'T', 'AGA':'R', 'AGC':'S', 'AGG':'R', 'AGU':'S', 'AUA':'I', 'AUC':'I', 'AUG':'M', 'AUU':'I', 'CAA':'Q', 'CAC':'H', 'CAG':'Q', 'CAU':'H', 'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCU':'P', 'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGU':'R', 'CUA':'L', 'CUC':'L', 'CUG':'L', 'CUU':'L', 'GAA':'E', 'GAC':'D', 'GAG':'E', 'GAU':'D', 'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCU':'A', 'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGU':'G', 'GUA':'V', 'GUC':'V', 'GUG':'V', 'GUU':'V', 'UAA':'.', 'UAC':'Y', 'UAG':'.', 'UAU':'Y', 'UCA':'S', 'UCC':'S', 'UCG':'S', 'UCU':'S', 'UGA':'.', 'UGC':'C', 'UGG':'W', 'UGU':'C', 'UUA':'L', 'UUC':'F', 'UUG':'L', 'UUU':'F' };
+	var Protein = '';
+	for(var i = 0; i < string.length; i +=3) Protein += (table[string.substr(i, 3)] ? table[string.substr(i, 3)] : '?');
+	return Protein;
+}
+exports.ProteinEncode = function(string)
+{
+	return exports.ProteinTranslation( string.replace(/T/g, 'U') );
+}
+
+// Peptide Encoding Problem: Find substrings of a genome encoding a given amino acid sequence.
+//      Input: A DNA string Text, an amino acid string Peptide, and the array GeneticCode.
+//      Output: All substrings of Text encoding Peptide (if any such substrings exist).
+// 
+// Example :
+// PeptideEncoding('ATGGCCATGGCCCCCAGAACTGAGATCAATAGTACCCGTATTAACGGGTGA', 'MA')
+exports.PeptideEncoding = function(string, peptide)
+{
+	var stringReverse = exports.ReverseComplement(string);
+	for(var i = 0, l = peptide.length, parts = []; i <= string.length + l; i++)
+	{
+		var part = string.substr(i, 3 * l);
+		var partReverse = stringReverse.substr(i, 3 * l);
+		if(exports.ProteinEncode( part ) == peptide) parts.push(part);
+		if(exports.ProteinEncode( partReverse ) == peptide) parts.push(exports.ReverseComplement(partReverse));
+	}
+	return parts;
+}
+
+// Example :
+// Find('ATGGCCATGGCCCCCAGAACTGAGATCAATAGTACCCGTATTAACGGGTGA', 'A')
+exports.Find = function(string, element)
+{
+	for(var i = 0, l = element.length, places = []; i <= string.length + l; i++)
+	{
+		if(string.substr(i, l) == element) places.push(i);
+	}
+	return places;
+}
+
+// Mass Spectrometry
+export.MassTable = { 'G':'57', 'A':'71', 'S':'87', 'P':'97', 'V':'99', 'T':'101', 'C':'103', 'I':'113', 'L':'113', 'N':'114', 'D':'115', 'K':'128', 'Q':'128', 'E':'129', 'M':'131', 'H':'137', 'F':'147', 'R':'156', 'Y':'163', 'W':'186' };
+
--- application-test.js
+++ application-test.js
@@ -6,6 +6,8 @@ console.log( Bio.HammingDistance('CTTGAAGTGGACCTCTAGTTCC', 'ATGCCTTACCTAGATGCAAT
 
 console.log( '\n> MinimumSkew' );
 console.log( Bio.MinimumSkew('CTTGAAGTGGACCTCTAGTTCC') );
+console.log( '\n> Skew' );
+console.log( Bio.Skew('CTTGAAGTGGACCTCTAGTTCC') );
 
 console.log( '\n> ReverseComplement' );
 console.log( 'CTTGAAGTGGACCTCTAGTTCC' );
@@ -17,4 +19,14 @@ console.log( JSON.stringify(Bio.Mismatches({'GGG' : 0}, 1)) );
 
 console.log( '\n> FrequentWordsMismatches' );
 console.log( Bio.FrequentWordsMismatches('ACGTTGCATGTCGCATGATGCATGAGAGCT', 4, 1) );
+
+console.log( '\n> ProteinTranslation' );
+console.log( Bio.ProteinTranslation('AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA') );
+
+console.log( '\n> PeptideEncoding' );
+console.log( Bio.PeptideEncoding('ATGGCCATGGCCCCCAGAACTGAGATCAATAGTACCCGTATTAACGGGTGA', 'MA') );
+
+console.log( '\n> Find' );
+console.log( Bio.Find('ATGGCCATGGCCCCCAGAACTGAGATCAATAGTACCCGTATTAACGGGTGA', 'A') );
+
 
